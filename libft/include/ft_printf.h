becc4c11817fb428596aef282f2994d87d012172
frueda-m <frueda-m@c3r4s5.42malaga.com>
2023-01-27 17:48:48 +0100

    Include ft_print in libf and update Makefile

---
+++ libft/include/ft_printf.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frueda-m <frueda-m@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/30 01:52:03 by frueda-m          #+#    #+#             */
+/*   Updated: 2022/12/31 20:07:30 by frueda-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_formats(va_list args, const char format);
+int		ft_printchar(int c);
+int		ft_printstr(char *str);
+int		ft_print_ptr(unsigned long long ptr);
+int		ft_printnbr(int n);
+int		ft_print_unsigned(unsigned int n);
+int		ft_print_hex(unsigned int num, const char format);
+
+void	ft_put_ptr(uintptr_t num);
+int		ft_ptr_len(uintptr_t num);
+char	*ft_uitoa(unsigned int n);
+int		ft_num_len(unsigned	int num);
+void	ft_put_hex(unsigned int num, const char format);
+int		ft_hex_len(unsigned	int num);
+int		ft_printpercent(void);
+
+#endif
